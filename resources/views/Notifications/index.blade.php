1f462e48e1d5c2f40801901a5234a2043026930d
Mandeep Gill <thisismyaim@gmail.com>
2016-04-12 15:08:30 +0530

    Notification Fix

--- resources/views/Notifications/index.blade.php
+++ resources/views/Notifications/index.blade.php
@@ -13,14 +13,35 @@
             <tbody>
             @if(count($notifications) > 0)
                 @foreach($notifications as $notification)
-                    <tr class="clickable {{ $notification->read == 0 ? "bold" : "blank" }} " data-link="{{ $notification->link }}" data-notify="{{ $notification->id }}">
-                        <td>{{ $notification->created_at->diffForHumans() }}</td>
-                        <td>{{ $notification->text }}</td>
-                        <td>{{ \App\User::find($notification->from)->name }}</td>
-                    </tr>
+                    
                 @endforeach
             @endif
             </tbody>
         </table>
     </div>
+@endsection
+@section('script')
+    <script>
+        $(".clickable").click(function() {
+            $that = $(this);
+            $.ajax({
+                url: '/api/makereadnotify',
+                method: 'POST',
+                data: {
+                    notification_id: $that.data('notify'),
+                    _token: "{{ csrf_token() }}"
+                },
+                success: function(data) {
+                    $.ajax({
+                        url: '/notifications/' + $that.data('notify'),
+                        method: 'DELETE',
+                        data: {
+                            _token: "{{ csrf_token() }}"
+                        }
+                    });
+                    window.location.href = $that.data('link');
+                }
+            });
+        })
+    </script>
 @endsection
