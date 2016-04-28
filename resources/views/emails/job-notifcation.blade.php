c7fc39d1f06490ef45c6b35872d71f503d74e122
Mandeep Gill <thisismyaim@gmail.com>
2016-04-28 14:46:12 +0530

    Revisions

--- resources/views/emails/job-notifcation.blade.php
+++ resources/views/emails/job-notifcation.blade.php
@@ -1,2 +1,2 @@
-<h1>You got a new job interview.</h1>
+<h1>You have a new job interview.</h1>
 <p><a href="http://yourserviceconnection.com/projects">Click Here</a> to view job and apply</p>
--- resources/views/emails/verify.blade.php
+++ resources/views/emails/verify.blade.php
@@ -1,4 +1,5 @@
 <h1>Thanks For Registration - Your Service Connection</h1>
 <p>Please Click Below Link to Activate Your Account as User</p>
 <p><a href="http://yourserviceconnection.com/auth/confirm?code={{$confirm}}">Confirm Account</a></p>
-<p>For more information please use our website support or contact us</p>
+<p>For more information please <a href="mailto:customerservice@yourserviceconnection.com
+">contact us</a></p>
