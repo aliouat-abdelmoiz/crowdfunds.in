c8a12096985861191c0365f6bed78f625da4fd4d
Mandeep Gill <thisismyaim@gmail.com>
2016-07-17 09:14:11 +0530

    Speed Up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>@yield('title')</title>
-    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate"/>
+    <meta http-equiv="Cache-control" content="public">
     <meta http-equiv="Pragma" content="no-cache"/>
     <meta http-equiv="Expires" content="30"/>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
@@ -198,8 +198,25 @@
             }
         };
     </script>
-    @endif
-    </div>
+@endif
+<?php if (!isset($_SERVER['HTTP_USER_AGENT']) || stripos($_SERVER['HTTP_USER_AGENT'], 'Speed Insights') === false): ?>
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+    ga('create', 'UA-53421234-1', 'auto');
+    ga('send', 'pageview');
+</script>
+<?php endif; ?>
+
 
 </body>
 </html>
