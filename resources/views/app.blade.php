fd57750cecbe58e6488786c02f9c961fca9eb666
Mandeep Gill <thisismyaim@gmail.com>
2016-07-17 09:45:44 +0530

    Speed Up

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <meta content="txHaVVh3gwpiIVTX8_-oR2D25IaXuednax3Cc5ZBQOI" name="google-site-verification"/>
     <link href="https://facebook.com/yourserviceconnection" rel="publisher"/>
     <!-- bower:css -->
-    
+
     <!-- endbower -->
     {!! Html::style('css/app.css') !!}
     {!! Html::style('css/style.min.css') !!}
