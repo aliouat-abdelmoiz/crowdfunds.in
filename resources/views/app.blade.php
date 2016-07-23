ad36f864c0259e919da76de1ff91c1cddd98385a
Mandeep Gill <thisismyaim@gmail.com>
2016-07-23 08:07:20 +0530

    Structure

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -192,14 +192,7 @@
 </div>
 
 <!-- bower:js -->
-<script src="/bower_components/jquery/dist/jquery.js"></script>
-<script src="/bower_components/bootstrap/dist/js/bootstrap.js"></script>
-<script src="/bower_components/vue/dist/vue.js"></script>
-<script src="/bower_components/jquery.maskedinput/dist/jquery.maskedinput.js"></script>
-<script src="/bower_components/sweetalert/dist/sweetalert.min.js"></script>
-<script src="/bower_components/jquery-infinite-scroll/jquery.infinitescroll.js"></script>
-<script src="/bower_components/vue-resource/dist/vue-resource.js"></script>
-<script src="/bower_components/dropzone/dist/min/dropzone.min.js"></script>
+
 <!-- endbower -->
 {!! Html::script('/lib/app.min.js') !!}
 @include('partial.notify')
